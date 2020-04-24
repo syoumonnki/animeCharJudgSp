db811da61d05b2088e774f0402537f01bf77dfe4
TakashiUsui <tk311anura@gmail.com>
2020-04-24 14:17:27 +0900

    選択画像の表示

--- src/main/java/com/animecharjudg/ResultController.java
+++ src/main/java/com/animecharjudg/ResultController.java
@@ -1,12 +1,32 @@
 package com.animecharjudg;
 
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-@Controller // (1)
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+@Controller
 public class ResultController {
-	@RequestMapping("/result") // (2)
-    public String toTop(Model model) {
+
+    @ModelAttribute
+    public ImageForm setForm() {
+        return new ImageForm();
+    }
+
+    @PostMapping("/result")
+    public String upload(ImageForm imageForm, Model model) throws Exception {
+
+        StringBuffer data = new StringBuffer();
+        String base64 = new String(Base64.encodeBase64(imageForm.getImage().getBytes()),"ASCII");
+        data.append("data:image/jpeg;base64,");
+        data.append(base64);
+
+        model.addAttribute("preview64image", data.toString());
+
+        model.addAttribute("fileName", "ファイル名：" + imageForm.getImage().getOriginalFilename());
+
+
         return "02_Result";
     }
+
 }
--- src/main/java/com/animecharjudg/TopController.java
+++ src/main/java/com/animecharjudg/TopController.java
@@ -2,12 +2,19 @@ package com.animecharjudg;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller // (1)
 public class TopController {
+
+    @ModelAttribute
+    public ImageForm setForm() {
+        return new ImageForm();
+    }
 
 	@RequestMapping("/top") // (2)
     public String toTop(Model model) {
         return "01_Top";
     }
+
 }
